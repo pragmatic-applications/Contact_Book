e3900d4fb7e3a741ccd9d2990ff8b9bb1b5dc0e1
Maurice Kelly <kay86@live.com>
2021-08-02 16:48:09 +0100

    First commit from VS

--- docs/Record.cs
+++ docs/Record.cs
@@ -2,20 +2,16 @@
 // Solution: Contact_Book
 //=================================================================
 // Programmer: Maurice Antonio Kelly @MAKMaurice
-// Description: A To Do App with undo, redo and database CRUD features.
+// Description: A Contact Book App with undo, redo and database CRUD features.
 // ASP.NET Core 5.0
 // Date: 2021-08-02
-// Stop: 14:00
+// Stop: 17:00
 //=================================================================
 
-// Add to GitHub...
-
-// Find: Lib_Core, Lib_Core_Interface
-
-// Todo: Separate Contact_Book from Item
+// First commit from VS
 
 //==========================================================================
-// Migrations S (2021-08-01)
+// Migrations S (2021-08-02)
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //
 // CLI
@@ -36,50 +32,6 @@
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 // Migrations E
 //==========================================================================
-
-/*//
-public static List<ContactEntityCategory> Categories
-{
-    get => new()
-    {
-        new ContactEntityCategory
-        {
-            Id = ++idCount,
-            Name = Enum.GetName(ContactCategoryType.Unspecified)
-        },
-        new ContactEntityCategory
-        {
-            Id = ++idCount,
-            Name = Enum.GetName(ContactCategoryType.Owner)
-        },
-        new ContactEntityCategory
-        {
-            Id = ++idCount,
-            Name = Enum.GetName(ContactCategoryType.Family)
-        },
-        new ContactEntityCategory
-        {
-            Id = ++idCount,
-            Name = Enum.GetName(ContactCategoryType.Friend)
-        },
-        new ContactEntityCategory
-        {
-            Id = ++idCount,
-            Name = Enum.GetName(ContactCategoryType.Home)
-        },
-        new ContactEntityCategory
-        {
-            Id = ++idCount,
-            Name = Enum.GetName(ContactCategoryType.Work)
-        },
-        new ContactEntityCategory
-        {
-            Id = ++idCount,
-            Name = Enum.GetName(ContactCategoryType.Business)
-        }
-    };
-}
-//*/
 
 /*
 
