5a38e09a5799983e2d90e3328c26c1ee6f29a9b1
Maurice Kelly <kay86@live.com>
2021-08-26 20:23:46 +0100

    Replace grid-template-columns: repeat(auto-fit, minmax(200px, 1fr)); with media queries for a more controlled layout.

--- MAK.Lib.Constant/Constants/CssData.cs
+++ MAK.Lib.Constant/Constants/CssData.cs
@@ -6,8 +6,10 @@
         public const string CardLinkCssCascadingValueThemeBlueA = "btn btn_theme_blue_a btn_sm btn_min_width";
         public const string CardLinkCssCascadingValueThemeBlueB = "btn btn_theme_blue_b text_theme_blue_b btn_sm btn_min_width";
 
-        public const string CardBodyCss = "grid_col_repeat_2_1fr";
-        public const string CardGroupCss = "grid_card_xs grid_card_md grid_card_lg";
+
+        public const string CardGroupCss = "grid_auto_fit_card_xs grid_md_2 grid_xl_4";
+        public const string CardBodyCss = "grid_auto_fit_card_xs grid_sm_2 grid_xl_1 grid_xxl_2";
+
 
         public const string CardCss = "card text_white theme_blue_a margin_top_10";
         public const string CardCssThemeBlueA = "card text_white theme_blue_a margin_top_10";
